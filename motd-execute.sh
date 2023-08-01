5d838cba8d87acd6f14cb59be4e4d7c28514d076
Aurel <ballinaurel@gmail.com>
2023-08-01 11:00:16 +0200

    update to match paths

--- motd-execute.sh
+++ motd-execute.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
+executable_path="$1"
 # This file is run by update-motd.d/1-execute
 # Read the contents of defaultmotd.txt into a variable
-default_motd=$(cat /etc/motdmanage/defaultmotd.txt)
+default_motd=$(cat "$executable_path"/defaultmotd.txt)
 
 # Echo both variables back to the console
 echo "$default_motd"
 # add some space
 echo ""
-#!/bin/bash
 
 # I have the following file path within my infofiles folder in /etc/motdmanage/infofiles:
 # folder
@@ -19,11 +19,11 @@ echo ""
 
 # get the list of folders in /etc/motdmanage/infofiles
 # loop through each folder
-for folder in /etc/motdmanage/infofiles/*; do
+for folder in "$executable_path"/infofiles/*; do
     # run the runnable.sh file
     # pass the folder path as an argument
     # the runnable.sh file should read the content.txt file and print it to the console
     # add an extra line for spacing
-    bash $folder/runnable.sh
+    bash "$folder"/runnable.sh
     echo ""
 done
